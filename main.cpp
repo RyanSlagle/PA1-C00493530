06ae45d559b2a842e1101f42badff223dfa57780
Snowghostx <ibzmhoztov@gmail.com>
2026-02-17 19:44:39 -0600

    Initial first 2 functions were done incorrectly. They have been adjusted correctly.

--- main.cpp
+++ main.cpp
@@ -3,24 +3,57 @@
 #include <string>
 using namespace std;
 
-void printGrid(int array[], int n) {
+void initializeForest(int array[], int n, int pFireCell) {
     for (int i = 0; i < n; i++) {
-        // i = the row we are at
         for (int j = 0; j < n; j++) {
-            //the columns
-            cout << "0 ";
+            if (rand() % 100 < 80) {
+                array[i*n + j] = 1;
+            }
+            else {
+                array[i*n + j] = 0;
+            }
         }
-        cout << endl;
+
+    }
+
+}
+
+/** printForest
+ * this function takes the nested loop function that prints out the grid and simply makes it easier to repeat.
+ * @param array an int array, the grid
+ * @param n an int that represents the grid size
+ */
+void printForest(int array[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        cout << "|";
+        for (int j = 0; j < n; j++) {
+            cout << array[i] << " ";
+        }
+        cout << "|" << endl;
     }
 }
 
 int main() {
     //initialize the forest
     int n;
-    cout << "Enter size of grid: " << endl;
+    cout << "Enter size of grid (an integer in range [10,30]: " << endl;
     cin >> n;
 
-    int forest_grid[n][n];
-    printGrid(*forest_grid, n);
+    int fireProb;
+    cout << "Enter probability of fire (an integer in range [10,90]: " << endl;
+    cin >> fireProb;
 
+    /*int spreadProb;
+    cout << "Enter probability of fire spread (an integer in range [25,100]: " << endl;
+    cin >> spreadProb;*/
+
+    int* forestGrid = new int[n*n];
+
+    initializeForest(forestGrid,n,fireProb);
+    printForest(forestGrid,n);
+
+
+
+    delete[] forestGrid; // no leakage
 }
